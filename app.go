0940da0ec1a06799e64be3e1f59280abd2f9edb6
Edward Suwirya <edward_suwirya@yahoo.com>
2022-05-17 09:51:45 +0700

    Go Binary Tree Hands On

---
+++ app.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+type node struct {
+	left  *node
+	right *node
+	data  int64
+}
+
+func (n *node) insert(data int64) {
+	if n == nil {
+		return
+	} else if data <= n.data {
+		if n.left == nil {
+			n.left = &node{data: data, left: nil, right: nil}
+		} else {
+			n.left.insert(data)
+		}
+	} else {
+		if n.right == nil {
+			n.right = &node{data: data, left: nil, right: nil}
+		} else {
+			n.right.insert(data)
+		}
+	}
+}
+
+type binaryTree struct {
+	root *node
+}
+
+func (t *binaryTree) insert(data int64) {
+	if t.root == nil {
+		t.root = &node{data: data, left: nil, right: nil}
+	} else {
+		t.root.insert(data)
+	}
+}
+
+func print(node *node, space int, ch string) {
+	if node == nil {
+		return
+	}
+
+	for i := 0; i < space; i++ {
+		fmt.Print(" ")
+	}
+	fmt.Printf("%s:%v\n", ch, node.data)
+	print(node.left, space+2, "L")
+	print(node.right, space+2, "R")
+}
+func main() {
+	tree := &binaryTree{}
+	tree.insert(100)
+	tree.insert(20)
+	tree.insert(120)
+	tree.insert(50)
+	tree.insert(15)
+	tree.insert(85)
+	print(tree.root, 0, "R")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module enigmacamp.com/tree
+
+go 1.17
